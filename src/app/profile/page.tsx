9632851d99295a4264a0189c35ae30e028b58941
hz <uongtradaokhong@gmail.com>
2026-01-18 08:41:43 +0700

    fix/ui-profile-layout: update profile page layout to 2-column grid - Use grid-cols-1 lg:grid-cols-2 for desktop layout - Align items to start to respect component heights

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,8 +8,18 @@ export default function ProfilePage() {
     <>
       <Navbar />
       <ProfileLayout>
-        <PersonalInfoSection />
-        <ChangePasswordSection />
+        {/* Layout 2 cột: Left (Info) - Right (Password) */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 h-full items-start">
+          {/* Cột trái: Thông tin cá nhân & Avatar */}
+          <div className="h-full">
+            <PersonalInfoSection />
+          </div>
+
+          {/* Cột phải: Đổi mật khẩu */}
+          <div className="h-full">
+            <ChangePasswordSection />
+          </div>
+        </div>
       </ProfileLayout>
     </>
   );
