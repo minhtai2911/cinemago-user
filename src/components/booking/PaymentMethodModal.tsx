ff9dc7d49405cc60eec4514aa94d0b123a9a5796
hz <uongtradaokhong@gmail.com>
2026-01-20 01:31:13 +0700

    fix/ui-booking: Improved the payment method popup interface, including adding a blurry popcorn image in the background and a linear effect to the main text

--- src/components/booking/PaymentMethodModal.tsx
+++ src/components/booking/PaymentMethodModal.tsx
@@ -17,7 +17,7 @@ export default function PaymentMethodModal({
   onSelect,
 }: PaymentMethodModalProps) {
   const [selectedMethod, setSelectedMethod] = useState<PaymentMethod | null>(
-    null
+    null,
   );
 
   useEffect(() => {
@@ -62,12 +62,16 @@ export default function PaymentMethodModal({
 
       <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
         <div className="bg-[#fffbf5] rounded-2xl shadow-2xl w-full max-w-[500px] aspect-square p-5 sm:p-8 flex flex-col justify-between animate-in fade-in zoom-in-95 duration-300 relative border border-white">
+          <div className="absolute inset-0 bg-[url('/corn.webp')] bg-cover bg-center opacity-8 pointer-events-none" />
           {/* THAY ĐỔI Ở ĐÂY: Thêm 'mb-4 sm:mb-8' */}
-          <h3 className="text-lg sm:text-2xl font-black text-gray-800 text-center uppercase whitespace-nowrap shrink-0 mb-4 sm:mb-8">
-            Chọn phương thức thanh toán
+          <h3 className="text-3xl md:text-3xl font-black text-center text-gray-900 mb-2 tracking-tight">
+            LỰA CHỌN{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#FF7043] to-[#FFAB91]">
+              PHƯƠNG THỨC THANH TOÁN
+            </span>
           </h3>
 
-          <div className="flex-1 flex flex-col justify-center gap-3 sm:gap-4 py-2">
+          <div className="flex-1 flex flex-col justify-center gap-3 sm:gap-4 px-3 py-3 mb-3">
             {methods.map((method) => (
               <button
                 key={method.id}
